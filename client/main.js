cc4e74500d6d415fd0bbeb3fe219429f1cd428b8
smettu007 <subbumetu007@gmail.com>
2018-05-07 18:25:49 -0700

    first section3 commit

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Meteor } from "meteor/meteor";
 import { Tracker } from "meteor/tracker";
-
+//this is branch section3
 import { Players } from "./../imports/api/players";
 
 const renderPlayers = function(playersList) {
